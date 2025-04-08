aa11796653e0ce51cc4b097281920a6d2a9bdd57
Flemming K. Jensen <fkj@eamv.dk>
2025-04-08 09:57:06 +0200

    Initial commit
    Dette er den første commit, hvor der kun er ændret i .gitignore, ellers er alt som IntelliJ har lavet det fra starten.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example._20251__javafx_med_git_6_ {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+
+  opens org.example._20251__javafx_med_git_6_ to javafx.fxml;
+  exports org.example._20251__javafx_med_git_6_;
+}
